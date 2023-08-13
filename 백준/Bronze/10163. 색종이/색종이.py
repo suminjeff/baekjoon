eb739faf8fb14b5239a9115996aae60eaf094b2d
이수민 <139415437+suminjeff@users.noreply.github.com>
2023-08-13 21:32:02 +0900

    [Bronze I] Title: 색종이, Time: 132 ms, Memory: 121452 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,23 +1,19 @@
 from sys import stdin
 
 N = int(stdin.readline())
+
+arr = [[0] * 1001 for _ in range(1001)]
+
+
 max_row = 0
 max_col = 0
-
-coor = []
-
 for i in range(1, N+1):
     c, r, nr, nc = map(int, stdin.readline().split())
     if max_row < r + nr:
         max_row = r + nr
     if max_col < c + nc:
         max_col = c + nc
-    coor.append([c, r, nr, nc])
 
-arr = [[0]*max_col for _ in range(max_row)]
-
-for i in range(1, N+1):
-    c, r, nr, nc = coor[i-1]
     for row in range(r, r+nr):
         for col in range(c, c+nc):
             arr[row][col] = i
