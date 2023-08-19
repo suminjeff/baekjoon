a38bbbb6ae083a3f04beef41cd0dbd1b2d3d228d
이수민 <139415437+suminjeff@users.noreply.github.com>
2023-08-19 18:07:20 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -0,0 +1,29 @@
+def solution(dartResult):
+    stack = []
+    point = {'S': 1, 'D': 2, 'T': 3}
+    # for i in dartResult:
+    i = 0
+    while i < len(dartResult):
+        if dartResult[i].isnumeric():
+            num = ""
+            while dartResult[i].isnumeric():
+                num += dartResult[i]
+                i += 1
+            stack.append(int(num))
+        elif dartResult[i] in point:
+            stack.append(stack.pop() ** point[dartResult[i]])
+            i += 1
+        else:
+            if dartResult[i] == "*":
+                if len(stack) > 1:
+                    a = stack.pop()
+                    b = stack.pop()
+                    stack.append(b * 2)
+                    stack.append(a * 2)
+                else:
+                    stack.append(stack.pop() * 2)
+            else:
+                stack.append(stack.pop() * (-1))
+            i += 1
+    answer = sum(stack)
+    return answer
