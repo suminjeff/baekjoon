eed5c94580db7b011a4269302962152a32d83115
이수민 <139415437+suminjeff@users.noreply.github.com>
2023-10-15 22:00:26 +0900

    [Gold V] Title: LCS, Time: 224 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+s1 = input().rstrip()
+s2 = input().rstrip()
+
+l1, l2 = len(s1), len(s2)
+
+dp = [0]*l2
+
+for i in range(l1):
+    cnt = 0
+    for j in range(l2):
+        if cnt < dp[j]:
+            cnt = dp[j]
+        elif s1[i] == s2[j]:
+            dp[j] = cnt + 1
+print(max(dp))
