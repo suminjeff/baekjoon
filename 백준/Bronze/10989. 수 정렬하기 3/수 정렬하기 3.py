de5261a0c84242c68757afff9a708f5fc215cddb
이수민 <139415437+suminjeff@users.noreply.github.com>
2023-10-27 17:21:46 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8716 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,11 +1,11 @@
 import sys
-input = sys.stdin.readline
+st = sys.stdin.readline
 
-N = int(input())
-cnt = [0] * 10001
+N = int(st())
+lst = [0] * 10001
 for i in range(N):
-    cnt[int(input())] += 1
-for j in range(10001):
-    if cnt[j]:
-        for k in range(cnt[j]):
-            print(j)
+    lst[int(st())] += 1
+
+for p in range(10001):
+    for _ in range(lst[p]):
+        print(p)
