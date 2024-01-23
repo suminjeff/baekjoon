d2d3bcc277de735c9c64961a6d1103f5b512c2ca
이수민 <139415437+suminjeff@users.noreply.github.com>
2024-01-23 23:39:10 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 72 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,32 +1,26 @@
 import sys
 input = sys.stdin.readline
-
 from collections import deque
 
-T = int(input())
-for tc in range(1, T+1):
-    N = int(input())
-    que = deque()
-    home = list(map(int, input().split()))
-    cvs = []
-    visited = [0]*N
-    for i in range(N):
-        x, y = map(int, input().split())
-        cvs.append([x, y])
-    gx, gy = list(map(int, input().split()))
-
-    que = deque()
-    que.append(home)
-    ans = "sad"
+# 9205
+t = int(input())
+max_distance = 20*50
+for _ in range(t):
+    n = int(input())
+    sx, sy = map(int, input().split())
+    cvs = [list(map(int, input().split())) for _ in range(n)]
+    ex, ey = map(int, input().split())
+    visited = [0]*n
+    que = deque([[sx, sy]])
+    answer = "sad"
     while que:
-        r, c = que.popleft()
-        if abs(r-gx) + abs(c-gy) <= 1000:
-            ans = "happy"
+        x, y = que.popleft()
+        if abs(x-ex) + abs(y-ey) <= max_distance:
+            answer = "happy"
             break
-        for i in range(N):
-            if not visited[i]:
-                nr, nc = cvs[i]
-                if abs(r-nr) + abs(c-nc) <= 1000:
-                    visited[i] = 1
-                    que.append([nr, nc])
-    print(ans)
+        for i in range(n):
+            cx, cy = cvs[i]
+            if visited[i] == 0 and abs(x-cx) + abs(y-cy) <= max_distance:
+                visited[i] = 1
+                que.append([cx, cy])
+    print(answer)
