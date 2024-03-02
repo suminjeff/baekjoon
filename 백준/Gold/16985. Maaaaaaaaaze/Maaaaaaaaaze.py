feb7a10a0d284f68ac99cf7db68f1899ca752a68
이수민 <139415437+suminjeff@users.noreply.github.com>
2024-03-02 09:51:37 +0900

    [Gold II] Title: Maaaaaaaaaze, Time: 1032 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.py
+++ 백준/Gold/16985. Maaaaaaaaaze/Maaaaaaaaaze.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
-from heapq import heappop, heappush
+from collections import deque
 
 
 # 16985
@@ -42,23 +42,21 @@ def bfs(arr):
     if arr[0][0][0] == 0 or arr[-1][-1][-1] == 0:
         return inf
 
-    visited = [[[inf for _ in range(5)] for _ in range(5)] for _ in range(5)]
+    visited = [[[-1 for _ in range(5)] for _ in range(5)] for _ in range(5)]
     visited[0][0][0] = 0
-    heap = [[0, 0, 0, 0]]
-    while heap:
-        d, h, r, c = heappop(heap)
-        if visited[h][r][c] < d:
-            continue
-        nd = d+1
+    que = deque([[0, 0, 0]])
+    while que:
+        h, r, c = que.popleft()
         for nh, nr, nc in [[h, r+1, c], [h, r-1, c], [h, r, c+1], [h, r, c-1], [h+1, r, c], [h-1, r, c]]:
-            if 0 <= nh < 5 and 0 <= nr < 5 and 0 <= nc < 5 and arr[nh][nr][nc] == 1 and visited[nh][nr][nc] > nd:
-                visited[nh][nr][nc] = nd
-                heappush(heap, [nd, nh, nr, nc])
-    return visited[-1][-1][-1]
+            if 0 <= nh < 5 and 0 <= nr < 5 and 0 <= nc < 5 and arr[nh][nr][nc] == 1 and visited[nh][nr][nc] == -1:
+                visited[nh][nr][nc] = visited[h][r][c] + 1
+                que.append([nh, nr, nc])
+    res = visited[-1][-1][-1]
+    return res if res != -1 else inf
 
 
 def solve(plate_order, spin_order):
-    global answer
+    answer = inf
     for p_order in plate_order:
         for s_order in spin_order:
             custom_maze = []
@@ -67,7 +65,8 @@ def solve(plate_order, spin_order):
             travel = bfs(custom_maze)
             answer = min(answer, travel)
             if answer == 12:
-                return
+                return answer
+    return answer
 
 
 inf = sys.maxsize
@@ -87,6 +86,5 @@ permutation(5, 5, 0, [], [0 for _ in range(5)])
 spin_order = []
 product(4, 5, 0, [])
 
-answer = inf
-solve(plate_order, spin_order)
+answer = solve(plate_order, spin_order)
 print(answer if answer != inf else -1)
