f5ec286e70791ca0b9bc0343a9f8138e2d92dd83
이수민 <139415437+suminjeff@users.noreply.github.com>
2024-03-31 17:31:49 +0900

    [Gold II] Title: 로봇 조종하기, Time: 284 ms, Memory: 128768 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
@@ -1,24 +1,34 @@
 import sys
 input = sys.stdin.readline
 
-# 2169 로봇 조종하기
+# 2169
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-dp = [[0]*M for _ in range(N)]
-dp[0][0] = arr[0][0]
-for c in range(1, M):
-    dp[0][c] = dp[0][c-1] + arr[0][c]
 
-for r in range(1, N):
-    LTR, RTL = arr[r][:], arr[r][:][::-1]
-    for c in range(M):
-        if c == 0:
-            LTR[c] += dp[r-1][c]
-            RTL[c] += dp[r-1][M-c-1]
-        else:
-            LTR[c] += max(LTR[c-1], dp[r-1][c])
-            RTL[c] += max(RTL[c-1], dp[r-1][M-c-1])
-    for c in range(M):
-        dp[r][c] = max(LTR[c], RTL[M-c-1])
+dp = [[0 for _ in range(M)] for _ in range(N)]
+dp[0][0] = arr[0][0]
+
+for i in range(1, M):
+    dp[0][i] = dp[0][i-1] + arr[0][i]
+
+for i in range(1, N):
+    row = [0]*M
+    row[0] = dp[i-1][0] + arr[i][0]
+
+    row_reversed = [0]*M
+    row_reversed[M-1] = dp[i-1][M-1] + arr[i][M-1]
+
+    for j in range(1, M):
+        # 정방향
+        row[j] = max(row[j-1], dp[i-1][j]) + arr[i][j]
+
+
+    for j in range(M-2, -1, -1):
+        # 역방향
+        row_reversed[j] = max(row_reversed[j+1], dp[i-1][j]) + arr[i][j]
+
+    for j in range(M):
+        dp[i][j] = max(row[j], row_reversed[j])
+
 print(dp[N-1][M-1])
