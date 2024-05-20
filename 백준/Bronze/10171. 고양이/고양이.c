60a9989e7764f68251ef9ec57b4938c7eb89648a
이수민 <139415437+suminjeff@users.noreply.github.com>
2024-05-20 23:19:48 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("%s\n", "\\    /\\");
+    printf("%s\n", " )  ( ')");
+    printf("%s\n", "(  /  )");
+    printf("%s\n", " \\(__)|");
+    
+    return 0;
+}
+
