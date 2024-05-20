c228b7f1a0dfddc33c22e8f15df3374104125712
이수민 <139415437+suminjeff@users.noreply.github.com>
2024-05-20 23:21:49 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("%s\n", "|\\_/|");
+    printf("%s\n", "|q p|   /}");
+    printf("%s\n", "( 0 )\"\"\"\\");
+    printf("%s\n", "|\"^\"`    |");
+    printf("%s\n", "||_/=\\\\__|");
+    
+    return 0;
+}
