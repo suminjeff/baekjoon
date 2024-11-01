ab5a94653f23d392c61b5a08deaef9632e9ec5ce
suminjeff <139415437+suminjeff@users.noreply.github.com>
2024-11-01 16:08:43 +0900

    [Silver II] Title: 사탕 게임, Time: 400 ms, Memory: 112456 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,39 +1,61 @@
-def get_max():
-    global max_len
-    for i in range(N):
-        length = 1
-        for j in range(N-1):
-            if arr[i][j] == arr[i][j+1]:
-                length += 1
-                if max_len < length:
-                    max_len = length
-            else:
-                length = 1
-    for j in range(N):
-        length = 1
-        for i in range(N-1):
-            if arr[i][j] == arr[i+1][j]:
-                length += 1
-                if max_len < length:
-                    max_len = length
-            else:
-                length = 1
-
+import sys
 
 N = int(input())
-arr = [list(input()) for _ in range(N)]
-max_len = 1
+candies = [list(input()) for _ in range(N)]
 
-for row in range(N):
-    for col in range(N-1):
-        arr[row][col], arr[row][col+1] = arr[row][col+1], arr[row][col]
-        get_max()
-        arr[row][col + 1], arr[row][col] = arr[row][col], arr[row][col + 1]
-for col in range(N):
-    for row in range(N-1):
-        arr[row][col], arr[row+1][col] = arr[row+1][col], arr[row][col]
-        get_max()
-        arr[row+1][col], arr[row][col] = arr[row][col], arr[row+1][col]
 
-get_max()
-print(max_len)
+
+def get_max_v():
+    max_v = 0
+    # 행 방향
+    for i in range(N):
+        color = candies[i][0]
+        v = 1
+        for j in range(1, N):
+            if candies[i][j] == color:
+                v += 1
+            else:
+                color = candies[i][j]
+                max_v = max(max_v, v)
+                v = 1
+        max_v = max(max_v, v)
+    # 열 방향
+    for j in range(N):
+        color = candies[0][j]
+        v = 1
+        for i in range(1, N):
+            if candies[i][j] == color:
+                v += 1
+            else:
+                color = candies[i][j]
+                max_v = max(max_v, v)
+                v = 1
+        max_v = max(max_v, v)
+    return max_v
+
+
+# 1단계: 초기에 모두 같은 색으로 이루어져 있는 가장 긴 연속 부분의 길이를 찾는다.
+max_v = get_max_v()
+
+# 2단계: (0, 0)부터 돌면서 서로 다른 색깔인 인접한 두 칸의 색깔을 바꾸고 max_v를 확인한다.
+# 행 방향
+for i in range(N):
+    for j in range(1, N):
+        a, b = candies[i][j-1], candies[i][j]
+        if a != b:
+            candies[i][j-1], candies[i][j] = candies[i][j], candies[i][j-1]  # 위치 바꾸기
+            tmp_max_v = get_max_v()  # 바뀐 상태에서 max_v 계산
+            max_v = max(max_v, tmp_max_v)  # max_v 갱신
+            candies[i][j-1], candies[i][j] = candies[i][j], candies[i][j-1]  # 위치 되돌려놓기
+
+# 열 방향
+for j in range(N):
+    for i in range(1, N):
+        a, b = candies[i-1][j], candies[i][j]
+        if a != b:
+            candies[i-1][j], candies[i][j] = candies[i][j], candies[i-1][j]  # 위치 바꾸기
+            tmp_max_v = get_max_v()  # 바뀐 상태에서 max_v 계산
+            max_v = max(max_v, tmp_max_v)  # max_v 갱신
+            candies[i-1][j], candies[i][j] = candies[i][j], candies[i-1][j]  # 위치 되돌려놓기
+
+print(max_v)
