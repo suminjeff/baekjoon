16756b3f362253d423ea01a9f35b9073ad755091
suminjeff <139415437+suminjeff@users.noreply.github.com>
2024-12-05 20:10:13 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 424 ms, Memory: 66452 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -5,6 +5,7 @@ from heapq import heappop, heappush
 
 inf = int(1e9)
 
+
 def dijkstra(start):
     distance = [inf] * (N+1)
     distance[start] = 0
@@ -32,12 +33,11 @@ for _ in range(E):
     adj_l[b].append([c, a])
 v1, v2 = map(int, input().split())
 
-distance = dijkstra(1)
 distance_v1 = dijkstra(v1)
 distance_v2 = dijkstra(v2)
 
-v1_first = distance[v1] + distance_v1[v2] + distance_v2[N]
-v2_first = distance[v2] + distance_v2[v1] + distance_v1[N]
+v1_first = distance_v1[1] + distance_v1[v2] + distance_v2[N]
+v2_first = distance_v2[1] + distance_v2[v1] + distance_v1[N]
 
 ans = min(v1_first, v2_first)
 print(ans if ans < inf else -1)
